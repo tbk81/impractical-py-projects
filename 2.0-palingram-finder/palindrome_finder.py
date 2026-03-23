c03b72b5d25a034d4151ca83599393bc14f3a08c
MBP <dev@tbk81.com>
2026-03-23 16:05:08 -0700

    palingrams

--- 2.0-palingram-finder/palindrome_finder.py
+++ 2.0-palingram-finder/palindrome_finder.py
@@ -8,4 +8,4 @@ for word in word_li:
     if len(word) > 1 and word == word[::-1]:
         pal_li.append(word)
 print(f'There were {len(pal_li)} palindromes found out of {len(word_li)} words')
-print(*pal_li, sep='\n')
+print(*pal_li, sep='\n')  # This will remove the quotes
