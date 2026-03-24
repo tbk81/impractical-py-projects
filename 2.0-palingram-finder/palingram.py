69115c79ae8c7f1c1f688df3de5320a1017650e6
MBP <dev@tbk81.com>
2026-03-24 16:37:50 -0700

    palingrams

--- 2.0-palingram-finder/palingram.py
+++ 2.0-palingram-finder/palingram.py
@@ -2,5 +2,15 @@
 
 from tools import load_dictionary
 
-def find_palingrams():
-    pass
+def find_palingrams(dict_txt):
+    """Find dictionary palingrams."""
+    word_li = load_dictionary.load(dict_txt)
+    pali_li = []
+
+    for word in word_li:
+        if len(word) > 1:
+            for l in word:
+                print(l)
+
+
+find_palingrams('2of12inf.txt')
