efc7771b36c530b29d6a66ce7d192cd2b37caf51
MBP <dev@tbk81.com>
2026-03-24 17:13:38 -0700

    palingrams optimized

--- 2.0-palingram-finder/palingram.py
+++ 2.0-palingram-finder/palingram.py
@@ -6,15 +6,16 @@ word_li = load_dictionary.load('2of12inf.txt')
 def find_palingrams():
     """Find dictionary palingrams."""
     pali_li = []
+    words = set(word_li)
 
-    for word in word_li:
+    for word in words:
         end = len(word)
         rev_word = word[::-1]
         if end > 1:
             for l in range(end):
-                if word[l:] == rev_word[:end - l] and rev_word[end - l:] in word_li:
+                if word[l:] == rev_word[:end - l] and rev_word[end - l:] in words:
                     pali_li.append((word, rev_word[end - l:]))
-                if word[:l] == rev_word[end - l:] and rev_word[:end - l] in word_li:
+                if word[:l] == rev_word[end - l:] and rev_word[:end - l] in words:
                     pali_li.append((rev_word[:end - l], word))
     return pali_li
 
