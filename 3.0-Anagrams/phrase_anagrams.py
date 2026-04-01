1ba2d6b4a8aea4b6a8be81714698dcfe6c7f18c0
MBP <dev@tbk81.com>
2026-04-01 10:33:44 -0700

    anagrams

---
+++ 3.0-Anagrams/phrase_anagrams.py
@@ -0,0 +1,27 @@
+"""
+Load a dictionary file
+Accept a name from user
+Set limit = length of name
+Start empty list to hold anagram phrase
+While length of phrase < limit:
+    Generate list of dictionary words that fit in name
+    Present words to user
+    Present remaining letters to user
+    Present current phrase to user
+    Ask user to input word or start over
+If user input can be made from remaining letters:
+    Accept choice of new word or words from user
+    Remove letters in choice from letters in name
+    Return choice and remaining letters in name
+If choice is not a valid selection:
+    Ask user for new choice or let user start over
+Add choice to phrase and show to user
+Generate new list of words and repeat process
+When phrase length equals limit value:
+Display final phrase
+Ask user to start over or to exit
+"""
+from tools import load_dictionary
+from collections import Counter
+
+
